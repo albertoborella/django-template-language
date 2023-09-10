4c571b5e1550fee2ee4570a018a31239bb3cdb86
Alberto F. Borella <113259191+albertoborella@users.noreply.github.com>
2023-09-10 17:43:13 -0300

    Add files via upload

--- ejercicios_practica/marvel/e_commerce/urls.py
+++ ejercicios_practica/marvel/e_commerce/urls.py
@@ -7,7 +7,19 @@ from e_commerce.views import *
 
 urlpatterns = [
     # NOTE: e_commerce base:
-    path('base', PruebaView.as_view()),
+    path('base', PruebaView.as_view(), name='base'),
     
     #TODO: Tarea! 
+    path('tabla', TablaView.as_view(), name='tabla'),
+    path('lista', ListaView.as_view(), name='lista'),
+    path('formulario', FormularioView.as_view(), name='formulario'),
+    path('texto', TextoView.as_view(), name='texto'),
+    path('imagen', ImagenView.as_view(), name='imagen'),
+    path('img-texto', ImgTextoView.as_view(), name='img-texto'),
+    path('login', LoginView.as_view(), name='login'),
+    path('datos_user', DatosUser.as_view(), name='datos_user'),
+    path('comics_favoritos', ComicsFavoritos.as_view(), name='comics_favoritos'),
+    path('carrito', Carrito.as_view(), name='carrito'),
+    path('gracias', Gracias.as_view(), name='gracias'),
+    path('agregar-comics', GuardarComicBD.as_view(), name='agregar-comics'),
     ]
--- ejercicios_practica/marvel/e_commerce/views.py
+++ ejercicios_practica/marvel/e_commerce/views.py
@@ -1,7 +1,10 @@
+from typing import Any
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from e_commerce.forms import ComicForm
 
 # Importamos vistas genericas:
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, ListView
 
 # Importamos los modelos que vamos a usar:
 from django.contrib.auth.models import User
@@ -9,3 +12,72 @@ from e_commerce.models import *
 
 class PruebaView(TemplateView):
     template_name = 'e-commerce/base.html'
+
+
+class TablaView(TemplateView):
+    template_name = 'e-commerce/1.mi_tabla.html'
+
+
+class ListaView(TemplateView):
+    template_name = 'e-commerce/2.mi_lista.html'
+
+
+class FormularioView(TemplateView):
+    template_name = 'e-commerce/3.mi_formulario.html'
+
+
+class TextoView(TemplateView):
+    template_name = 'e-commerce/4.mi_texto.html'
+
+
+class ImagenView(TemplateView):
+    template_name = 'e-commerce/5.mi_imagen.html'
+
+
+class ImgTextoView(TemplateView):
+    template_name = 'e-commerce/img-texto.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'e-commerce/login.html'
+
+class DatosUser(TemplateView):
+    model = User
+    template_name = 'e-commerce/user.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = self.request.user.username
+        context['first_name'] = self.request.user.first_name
+        context['last_name'] = self.request.user.last_name
+        context['email'] = self.request.user.email
+        return context
+    
+
+class ComicsFavoritos(TemplateView):
+    template_name = 'e-commerce/wish.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comic_favorito'] = ['Titulo 1','Titulo 5','Titulo 8', 'Titulo 12', 'Título 14']
+        return context
+
+
+class GuardarComicBD(CreateView):
+    model = Comic
+    fields = '__all__'
+    template_name = 'e-commerce/guardar_comic_bd.html'
+
+
+
+class Carrito(TemplateView):
+    template_name = 'e-commerce/cart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comic_carrito'] = ['Titulo 1','Titulo 8', 'Título 14']
+        return context
+
+
+class Gracias(TemplateView):
+    template_name = 'e-commerce/thanks.html'
